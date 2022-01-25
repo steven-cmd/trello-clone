caa36c94ec3162bcd38e5941ad190d7c9de1a1bd
steven-cmd <stevenedclark@gmail.com>
2022-01-24 22:09:52 -0700

    first commit

---
+++ src/state/appStateReducer.ts
@@ -0,0 +1,51 @@
+import { Action } from "./actions";
+import { nanoid } from "nanoid";
+import { findItemIndexById, moveItem } from "../utils/arrayUtils";
+
+type Task = {
+  id: string;
+  text: string;
+};
+
+type List = {
+  id: string;
+  text: string;
+  tasks: Task[];
+};
+
+export type AppState = {
+  lists: List[];
+};
+
+export const appStateReducer = (
+  draft: AppState,
+  action: Action
+): AppState | void => {
+  switch (action.type) {
+    case "ADD_LIST": {
+      draft.lists.push({
+        id: nanoid(),
+        text: action.payload,
+        tasks: [],
+      });
+      break;
+    }
+    case "ADD_TASK": {
+      const { text, listId } = action.payload;
+      const targetListIndex = findItemIndexById(draft.lists, listId);
+
+      draft.lists[targetListIndex].tasks.push({
+        id: nanoid(),
+        text,
+      });
+      break;
+    }
+    case "MOVE_LIST": {
+      const { draggedId, hoverId } = action.payload;
+      const dragIndex = findItemIndexById(draft.lists, draggedId);
+      const hoverIndex = findItemIndexById(draft.lists, hoverId);
+      draft.lists = moveItem(draft.lists, dragIndex, hoverIndex);
+      break;
+    }
+  }
+};
